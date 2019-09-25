a9a7351be837fbe995d052441f8d53a323d16294
Alex Faissal <alefaissal@gmail.com>
2019-09-25 08:09:58 -0700

    Add skills and project with flickity

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 
+
 // All JS/jQuery and AJAX goes inside this function
 $(function () {
-    // const $timesMenu = $('#times-menu');
+
+ 
+
+
+  //skills animation
+  $('.bar').each(function () {
+    // if(window.innerHeight){
+    $(this).find('.bar-inner').animate({
+      width: $(this).attr('data-width')
+    }, 2000);
+  // }
+  });
 
 
 
 
 
 
-    
-    
 
-        
-}); //end of document ready
+
+
+
+}); //(jQuery);
